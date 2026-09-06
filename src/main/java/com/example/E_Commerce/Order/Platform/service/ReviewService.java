9ce097bd8013faf6efeff8c3d03b60509c951dad
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:36:16 +0400

    Create ReviewService

---
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/ReviewService.java
@@ -0,0 +1,13 @@
+package com.example.E_Commerce.Order.Platform.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ReviewService implements CrudService<ReviewDTO> {
+
+
+}
+
