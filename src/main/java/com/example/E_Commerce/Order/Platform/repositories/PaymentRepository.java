e47cbdc77a4468981199370371e11edf38510990
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:44:19 +0400

    Add method to get all active payments

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/PaymentRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/PaymentRepository.java
@@ -3,7 +3,12 @@ package com.example.E_Commerce.Order.Platform.repositories;
 import com.example.E_Commerce.Order.Platform.entities.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+
+    List<Payment> findAllByIsActiveTrue();
 
 }
 
