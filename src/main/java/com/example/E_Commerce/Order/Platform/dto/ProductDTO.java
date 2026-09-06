f9c512cfa644147546cf1d28ad5dd81ec623a1e0
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 15:31:14 +0400

    Add Product entity to DTO conversion

--- src/main/java/com/example/E_Commerce/Order/Platform/dto/ProductDTO.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/dto/ProductDTO.java
@@ -1,5 +1,6 @@
 package com.example.E_Commerce.Order.Platform.dto;
 
+import com.example.E_Commerce.Order.Platform.entities.Product;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,5 +18,15 @@ public class ProductDTO {
     private Integer stockQuantity;
     private String sku;
 
+    public static ProductDTO convertToDTO(Product product) {
+
+        return ProductDTO.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .price(product.getPrice())
+                .stockQuantity(product.getStockQuantity())
+                .sku(product.getSku())
+                .build();
+    }
 }
 
