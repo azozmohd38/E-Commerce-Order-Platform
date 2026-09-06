322dd65e4db98a9e030db0fa58e4d905c2a2ad29
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:27:47 +0400

    Add OrderItem CRUD operations

--- src/main/java/com/example/E_Commerce/Order/Platform/service/OrderItemService.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/OrderItemService.java
@@ -1,9 +1,13 @@
 package com.example.E_Commerce.Order.Platform.service;
 
+import com.example.E_Commerce.Order.Platform.entities.OrderItem;
 import com.example.E_Commerce.Order.Platform.repositories.OrderItemRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static java.nio.file.Files.find;
+import static java.util.Collections.copy;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +15,45 @@ import org.springframework.stereotype.Service;
 public class OrderItemService implements CrudService<OrderItemDTO> {
 
     private final OrderItemRepository repo;
+    public OrderItemDTO create(OrderItemDTO d) {
+
+        OrderItem e = new OrderItem();
+
+        copy(d, e);
+
+        e.setIsActive(true);
+
+        return OrderItemDTO.convertToDTO(
+                repo.save(e)
+        );
+    }
+
+
+    public List<OrderItemDTO> getAll() {
+
+        return OrderItemDTO.convertToDTO(
+                repo.findAllByIsActiveTrue()
+        );
+    }
+
+
+    public OrderItemDTO getById(Long id) {
+
+        return OrderItemDTO.convertToDTO(
+                find(id)
+        );
+    }
+
+
+    public OrderItemDTO update(Long id, OrderItemDTO d) {
+
+        OrderItem e = find(id);
+
+        copy(d, e);
+
+        return OrderItemDTO.convertToDTO(
+                repo.save(e)
+        );
+    }
+
 }
