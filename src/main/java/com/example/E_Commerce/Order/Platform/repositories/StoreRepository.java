de7bf5e5ab3233a03c0fc7ca542d52f59aeba90d
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:22:54 +0400

    modifi (Add method to get all active stores)

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/StoreRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/StoreRepository.java
@@ -3,21 +3,17 @@ package com.example.E_Commerce.Order.Platform.repositories;
 
 import com.example.E_Commerce.Order.Platform.entities.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
-
-@Query("SELECT s FROM Store s WHERE s.isActive = true")
-List<Store> getAllStores();
+    List<Store> findAllByIsActiveTrue();
 
 
 
-@Query("SELECT s FROM Store s WHERE s.id = :id AND s.isActive = true")
-    Store getStoreById(@Param("id") Long id);
+
 
 
 
