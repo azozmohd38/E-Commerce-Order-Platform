58c5f8c7b84a1be52b9f193b4abf07b181158cb2
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 16:25:32 +0400

    Add ReviewDTO fields

--- src/main/java/com/example/E_Commerce/Order/Platform/dto/ReviewDTO.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/dto/ReviewDTO.java
@@ -1,14 +1,22 @@
 package com.example.E_Commerce.Order.Platform.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReviewDTO {
+
+    private Long id;
+    private Integer rating;
+    private String comment;
+    private Date reviewDate;
 
 }
 
