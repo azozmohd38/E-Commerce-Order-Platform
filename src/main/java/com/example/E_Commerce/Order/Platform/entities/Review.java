2db62e6ad95496e4a31deb85c8d5c9ef99193eb0
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 10:59:45 +0400

    Add Review basic fields

--- src/main/java/com/example/E_Commerce/Order/Platform/entities/Review.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/entities/Review.java
@@ -2,13 +2,20 @@ package com.example.E_Commerce.Order.Platform.entities;
 
 
 import jakarta.persistence.Entity;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Date;
 
 @Entity
 @Getter
 @Setter
 public class Review extends BaseClass{
+
+    private Integer rating;
+    private String comment;
+    private Date reviewDate;
 
 
 }
