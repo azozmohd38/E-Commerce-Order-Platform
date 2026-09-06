8fe48fa7541cd9fb6b8b10b77ded616e317e8728
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:44:51 +0400

    Add method to get active payment by ID

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/PaymentRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/PaymentRepository.java
@@ -4,11 +4,13 @@ import com.example.E_Commerce.Order.Platform.entities.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
 
     List<Payment> findAllByIsActiveTrue();
+    Optional<Payment> findByIdAndIsActiveTrue(Long id);
 
 }
 
