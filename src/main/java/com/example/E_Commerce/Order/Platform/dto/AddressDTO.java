6defb2ed46c83f688d54a7ebe3c9bffcfc03508f
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 16:08:56 +0400

    Add Address entity to DTO conversion

--- src/main/java/com/example/E_Commerce/Order/Platform/dto/AddressDTO.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/dto/AddressDTO.java
@@ -1,5 +1,6 @@
 package com.example.E_Commerce.Order.Platform.dto;
 
+import com.example.E_Commerce.Order.Platform.entities.Address;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,4 +17,14 @@ public class AddressDTO {
     private String city;
     private String postalCode;
     private String type;
+    public static AddressDTO convertToDTO(Address address) {
+
+        return AddressDTO.builder()
+                .id(address.getId())
+                .street(address.getStreet())
+                .city(address.getCity())
+                .postalCode(address.getPostalCode())
+                .type(address.getType())
+                .build();
+    }
 }
