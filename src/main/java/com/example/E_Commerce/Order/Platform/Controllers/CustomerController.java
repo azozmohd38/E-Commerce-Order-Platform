b9a1ebeadd116682df01c7f11f02b3bc3dca596e
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:51:53 +0400

    Add create Customer endpoint

--- src/main/java/com/example/E_Commerce/Order/Platform/Controllers/CustomerController.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/CustomerController.java
@@ -2,6 +2,8 @@ package com.example.E_Commerce.Order.Platform.Controllers;
 
 import com.example.E_Commerce.Order.Platform.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,11 @@ public class CustomerController {
 
     private final CustomerService service;
 
+    @PostMapping("add")
+    public CustomerDTO create(@RequestBody CustomerDTO d) {
+
+        return service.create(d);
+    }
 
 }
 
