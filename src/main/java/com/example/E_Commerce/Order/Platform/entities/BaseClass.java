691d8a2954316b7f5740848aa43e451c390501b7
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 09:15:27 +0400

    add create, update and isActive data

--- src/main/java/com/example/E_Commerce/Order/Platform/entities/BaseClass.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/entities/BaseClass.java
@@ -4,11 +4,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.Date;
+
 public class BaseClass {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
+
+    private Boolean isActive;
+    private Date createdDate;
+    private Date updatedDate;
 
 
 }
