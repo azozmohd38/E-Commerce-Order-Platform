95ef7af5282d7dc1f84b9524bc4189c2886eee1e
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:48:13 +0400

    Add method to get all active reviews

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/ReviewRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/ReviewRepository.java
@@ -3,7 +3,12 @@ package com.example.E_Commerce.Order.Platform.repositories;
 import com.example.E_Commerce.Order.Platform.entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+
+    List<Review> findAllByIsActiveTrue();
 
 }
 
