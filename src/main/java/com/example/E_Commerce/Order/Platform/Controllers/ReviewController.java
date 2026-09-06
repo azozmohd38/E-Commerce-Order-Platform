75d7af59064d79759f31a2ff9dd8452bc48e32b0
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 15:18:17 +0400

    Inject ReviewService into ReviewController

--- src/main/java/com/example/E_Commerce/Order/Platform/Controllers/ReviewController.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/ReviewController.java
@@ -1,5 +1,6 @@
 package com.example.E_Commerce.Order.Platform.Controllers;
 
+import com.example.E_Commerce.Order.Platform.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("review")
 @RequiredArgsConstructor
 public class ReviewController {
+
+    private final ReviewService service;
+
 
 }
 
