db40ae59f49325bf8a9906950acabbd974f0ecfc
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 15:08:54 +0400

    Inject OrderItemService into OrderItemController

--- src/main/java/com/example/E_Commerce/Order/Platform/Controllers/OrderItemController.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/OrderItemController.java
@@ -1,5 +1,6 @@
 package com.example.E_Commerce.Order.Platform.Controllers;
 
+import com.example.E_Commerce.Order.Platform.service.OrderItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,10 @@ RestController
 @RequestMapping("orderItem")
 @RequiredArgsConstructor
 public class OrderItemController {
+
+    private final OrderItemService service;
+
+
 
 }
 
