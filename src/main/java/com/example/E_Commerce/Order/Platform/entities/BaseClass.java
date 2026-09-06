4267dead6aff51180ffafc05cb47c6cd99310fde
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 09:12:43 +0400

    add ID automatic generation

--- src/main/java/com/example/E_Commerce/Order/Platform/entities/BaseClass.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/entities/BaseClass.java
@@ -1,4 +1,14 @@
 package com.example.E_Commerce.Order.Platform.entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class BaseClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long id;
+
+
 }
