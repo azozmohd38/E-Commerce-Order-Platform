250358027b2898b6f64f280c1bd099c14e88f2db
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:26:34 +0400

    Create OrderItemService

---
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/OrderItemService.java
@@ -0,0 +1,13 @@
+package com.example.E_Commerce.Order.Platform.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderItemService implements CrudService<OrderItemDTO> {
+
+
+}
