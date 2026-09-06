f1b08a2afd5bedae407fe9ffa64260a31f07be33
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 17:06:18 +0400

    ProductRepository copmplited

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/ProductRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/ProductRepository.java
@@ -2,13 +2,45 @@ package com.example.E_Commerce.Order.Platform.repositories;
 
 import com.example.E_Commerce.Order.Platform.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-
     List<Product> findAllByIsActiveTrue();
+
     Optional<Product> findByIdAndIsActiveTrue(Long id);
+
+    @Query("""
+            SELECT p
+            FROM Product p
+            WHERE p.category.id = :categoryId
+            AND p.isActive = true
+            """)
+    List<Product> getActiveProductsByCategory(
+            @Param("categoryId") Long categoryId
+    );
+
+    @Query("""
+            SELECT p
+            FROM Product p
+            WHERE p.price < :price
+            AND p.isActive = true
+            """)
+    List<Product> getProductsBelowPrice(
+            @Param("price") Double price
+    );
+
+    @Query("""
+            SELECT p
+            FROM Product p
+            WHERE p.stockQuantity < :threshold
+            AND p.isActive = true
+            """)
+    List<Product> getLowStockProducts(
+            @Param("threshold") Integer threshold
+    );
 }
