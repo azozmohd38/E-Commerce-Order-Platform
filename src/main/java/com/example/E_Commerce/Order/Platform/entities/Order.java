9c02e5335721a92c8de88bf7a0064c1a90237be3
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 10:44:09 +0400

    Add Order basic fields

--- src/main/java/com/example/E_Commerce/Order/Platform/entities/Order.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/entities/Order.java
@@ -5,10 +5,16 @@ import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Entity
 @Getter
 @Setter
 public class Order extends BaseClass {
+
+    private Date orderDate;
+    private String status;
+    private Double totalAmount;
 
 
 
