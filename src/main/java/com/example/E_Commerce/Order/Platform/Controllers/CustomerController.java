251000fff10d53a20623cde58e65c086d9a82c35
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:52:12 +0400

    Add get all Customers endpoint

--- src/main/java/com/example/E_Commerce/Order/Platform/Controllers/CustomerController.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/CustomerController.java
@@ -2,10 +2,7 @@ package com.example.E_Commerce.Order.Platform.Controllers;
 
 import com.example.E_Commerce.Order.Platform.service.CustomerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("customer")
@@ -18,6 +15,11 @@ public class CustomerController {
     public CustomerDTO create(@RequestBody CustomerDTO d) {
 
         return service.create(d);
+    }
+    @GetMapping("getAll")
+    public List<CustomerDTO> getAll() {
+
+        return service.getAll();
     }
 
 }
