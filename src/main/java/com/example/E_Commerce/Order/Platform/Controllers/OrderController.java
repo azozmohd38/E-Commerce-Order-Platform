c0083867c01a022cc0724dd26f1b77fe914ec8ae
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 15:06:05 +0400

    Add create Order endpoint

--- src/main/java/com/example/E_Commerce/Order/Platform/Controllers/OrderController.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/OrderController.java
@@ -2,6 +2,8 @@ package com.example.E_Commerce.Order.Platform.Controllers;
 
 import com.example.E_Commerce.Order.Platform.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final OrderService service;
+
+    @PostMapping("add")
+    public OrderDTO create(@RequestBody OrderDTO d) {
+
+        return service.create(d);
+    }
 
 
 }
