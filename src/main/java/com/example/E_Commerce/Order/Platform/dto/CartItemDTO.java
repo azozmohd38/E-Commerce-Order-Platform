d66b7a226df0ab5d0461f68c38fe8ea243c619e8
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 16:13:45 +0400

    Add CartItem entity to DTO conversion

--- src/main/java/com/example/E_Commerce/Order/Platform/dto/CartItemDTO.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/dto/CartItemDTO.java
@@ -1,5 +1,6 @@
 package com.example.E_Commerce.Order.Platform.dto;
 
+import com.example.E_Commerce.Order.Platform.entities.CartItem;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +14,14 @@ public class CartItemDTO {
 
     private Long id;
     private Integer quantity;
+
+    public static CartItemDTO convertToDTO(CartItem cartItem) {
+
+        return CartItemDTO.builder()
+                .id(cartItem.getId())
+                .quantity(cartItem.getQuantity())
+                .build();
+    }
 
 }
 
