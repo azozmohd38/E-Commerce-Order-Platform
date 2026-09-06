5a2ead734d521f482bc87282a8025f1c3d626c1f
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:27:08 +0400

    Add OrderItemRepository dependency

--- src/main/java/com/example/E_Commerce/Order/Platform/service/OrderItemService.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/OrderItemService.java
@@ -1,5 +1,6 @@
 package com.example.E_Commerce.Order.Platform.service;
 
+import com.example.E_Commerce.Order.Platform.repositories.OrderItemRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,5 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class OrderItemService implements CrudService<OrderItemDTO> {
 
-
+    private final OrderItemRepository repo;
 }
