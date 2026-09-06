56c004b997c7e3a929bdef181c0dedb653c5d637
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 16:35:25 +0400

    Add method to find active CartItem by Cart and Product in CartResponse

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/CartRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/CartRepository.java
@@ -1,6 +1,8 @@
 package com.example.E_Commerce.Order.Platform.repositories;
 
 import com.example.E_Commerce.Order.Platform.entities.Cart;
+import com.example.E_Commerce.Order.Platform.entities.CartItem;
+import com.example.E_Commerce.Order.Platform.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,5 +13,8 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
     List<Cart> findAllByIsActiveTrue();
     Optional<Cart> findByIdAndIsActiveTrue(Long id);
 
-
+    Optional<CartItem> findByCartAndProductAndIsActiveTrue(
+            Cart cart,
+            Product product
+    );
 }
