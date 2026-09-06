f966451bc0d0abc9c63ade15d2c1491b49e237a8
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:17:18 +0400

    Add query to get active store by ID

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/StoreRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/StoreRepository.java
@@ -4,6 +4,7 @@ package com.example.E_Commerce.Order.Platform.repositories;
 import com.example.E_Commerce.Order.Platform.entities.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,6 +16,8 @@ List<Store> getAllStores();
 
 
 
+@Query("SELECT s FROM Store s WHERE s.id = :id AND s.isActive = true")
+    Store getStoreById(@Param("id") Long id);
 
 
 
