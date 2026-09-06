e3cdd3385b9158a58535e12fe20eaf9cf977a485
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 10:24:18 +0400

    Add Customer and Cart relationship

--- src/main/java/com/example/E_Commerce/Order/Platform/entities/Customer.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/entities/Customer.java
@@ -1,7 +1,10 @@
 package com.example.E_Commerce.Order.Platform.entities;
 
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,6 +18,9 @@ public class Customer extends BaseClass {
     private String email;
     private String phoneNumber;
     private String gender;
+
+    @OneToOne(mappedBy = "customer", cascade = CascadeType.ALL)
+    private Cart cart;
 
 
 }
