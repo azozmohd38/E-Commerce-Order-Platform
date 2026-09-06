c2ebbae33f6107c3692a64442ba3e8f75b393e5c
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:57:30 +0400

    Inject CartService into CartController

--- src/main/java/com/example/E_Commerce/Order/Platform/Controllers/CartController.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/CartController.java
@@ -1,5 +1,6 @@
 package com.example.E_Commerce.Order.Platform.Controllers;
 
+import com.example.E_Commerce.Order.Platform.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("cart")
 @RequiredArgsConstructor
 public class CartController {
+
+    private final CartService service;
+
 
 
 }
