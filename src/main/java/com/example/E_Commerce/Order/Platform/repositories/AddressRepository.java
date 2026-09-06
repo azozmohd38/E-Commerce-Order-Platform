62c2ce1ab262de99a994534db5246526fdb92c7b
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:33:44 +0400

    Add method to get active address by ID

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/AddressRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/AddressRepository.java
@@ -4,10 +4,13 @@ import com.example.E_Commerce.Order.Platform.entities.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
     List<Address> findAllByIsActiveTrue();
+    Optional<Address> findByIdAndIsActiveTrue(Long id);
+
 
 
 }
