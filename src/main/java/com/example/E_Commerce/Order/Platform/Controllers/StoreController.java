a741bf387f3725db1f576fe6bd1fc4d2a73e506d
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:44:18 +0400

    Add create Category endpoint

--- src/main/java/com/example/E_Commerce/Order/Platform/Controllers/StoreController.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/StoreController.java
@@ -2,6 +2,8 @@ package com.example.E_Commerce.Order.Platform.Controllers;
 
 import com.example.E_Commerce.Order.Platform.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +14,9 @@ public class CategoryController {
 
     private final CategoryService service;
 
+    @PostMapping("add")
+    public CategoryDTO create(@RequestBody CategoryDTO d) {
+
+        return service.create(d);
+    }
 }
