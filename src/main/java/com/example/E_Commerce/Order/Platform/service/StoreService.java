9a26fdf9683b55eaf97935d898f2766fdec5fb9c
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 12:19:39 +0400

    Add Store DTO copy method

--- src/main/java/com/example/E_Commerce/Order/Platform/service/StoreService.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/StoreService.java
@@ -19,9 +19,7 @@ import static java.util.Collections.copy;
 public class StoreService implements CrudService<StoreDTO> {
 
 
-
     private final StoreRepository repo;
-
 
 
     public StoreDTO create(StoreDTO d) {
@@ -29,25 +27,36 @@ public class StoreService implements CrudService<StoreDTO> {
         copy(d, e);
         return StoreDTO.convertToDTO(repo.save(e));
     }
+
     public List<StoreDTO> getAll() {
         return StoreDTO.convertToDTO(repo.findAllByIsActiveTrue());
     }
+
     public StoreDTO getById(Long id) {
         return StoreDTO.convertToDTO(find(id));
     }
+
     public StoreDTO update(Long id, StoreDTO d) {
         Store e = find(id);
         copy(d, e);
         return StoreDTO.convertToDTO(repo.save(e));
 
     }
+
     public void delete(Long id) {
         Store e = find(id);
         e.setIsActive(false);
         repo.save(e);
     }
+
     Store find(Long id) {
         return EntityHelper.active(repo, id, "Store");
     }
+
+    private void copy(StoreDTO d, Store e) {
+        e.setName(d.getName());
+        e.setLocation(d.getLocation());
+    }
+}
 
 
