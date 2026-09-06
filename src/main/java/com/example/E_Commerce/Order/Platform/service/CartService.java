ea98d4f301902b80e8660f62b014ca523c87ab7c
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:17:23 +0400

    Add CartRepository dependency

--- src/main/java/com/example/E_Commerce/Order/Platform/service/CartService.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/CartService.java
@@ -1,5 +1,6 @@
 package com.example.E_Commerce.Order.Platform.service;
 
+import com.example.E_Commerce.Order.Platform.repositories.CartRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,6 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class CartService implements CrudService<CartDTO> {
 
-
+    private final CartRepository repo;
 }
 
