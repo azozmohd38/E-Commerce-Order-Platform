ebd73a47dd6aa6e8572c7251fae360bca8d9e22f
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:14:30 +0400

    Add Address CRUD operation

--- src/main/java/com/example/E_Commerce/Order/Platform/service/AddressService.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/AddressService.java
@@ -1,10 +1,14 @@
 package com.example.E_Commerce.Order.Platform.service;
 
 
+import com.example.E_Commerce.Order.Platform.entities.Address;
 import com.example.E_Commerce.Order.Platform.repositories.AddressRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static java.nio.file.Files.find;
+import static java.util.Collections.copy;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +17,42 @@ public class AddressService implements CrudService<AddressDTO> {
 
     private final AddressRepository repo;
 
+    public AddressDTO create(AddressDTO d) {
 
+        Address e = new Address();
+
+        copy(d, e);
+
+        e.setIsActive(true);
+
+        return AddressDTO.convertToDTO(repo.save(e));
+    }
+
+
+    public List<AddressDTO> getAll() {
+
+        return AddressDTO.convertToDTO(
+                repo.findAllByIsActiveTrue()
+        );
+    }
+
+
+    public AddressDTO getById(Long id) {
+
+        return AddressDTO.convertToDTO(
+                find(id)
+        );
+    }
+
+
+    public AddressDTO update(Long id, AddressDTO d) {
+
+        Address e = find(id);
+
+        copy(d, e);
+
+        return AddressDTO.convertToDTO(
+                repo.save(e)
+        );
+    }
 }
