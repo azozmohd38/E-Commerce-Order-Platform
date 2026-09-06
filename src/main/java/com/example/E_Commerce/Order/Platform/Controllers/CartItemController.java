d16227f26549ef128f99017f7972b4e7ed07dc8c
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 15:01:53 +0400

    Add get all CartItems endpoint

--- src/main/java/com/example/E_Commerce/Order/Platform/Controllers/CartItemController.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/CartItemController.java
@@ -2,10 +2,7 @@ package com.example.E_Commerce.Order.Platform.Controllers;
 
 import com.example.E_Commerce.Order.Platform.service.CartItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("cartItem")
@@ -18,6 +15,12 @@ public class CartItemController {
 
         return service.create(d);
     }
+    @GetMapping("getAll")
+    public List<CartItemDTO> getAll() {
+
+        return service.getAll();
+    }
+
 
 }
 
