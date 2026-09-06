6efa13dd7b35c47b350cb4b62b4d7d9242f3d299
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 12:21:27 +0400

    Create CategoryService

---
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/CategoryService.java
@@ -0,0 +1,22 @@
+package com.example.E_Commerce.Order.Platform.service;
+
+import com.example.E_Commerce.Order.Platform.dto.*;
+import com.example.E_Commerce.Order.Platform.entities.*;
+import com.example.E_Commerce.Order.Platform.repositories.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
+
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CategoryService implements CrudService<CategoryDTO> {
+
+
+}
+
+
