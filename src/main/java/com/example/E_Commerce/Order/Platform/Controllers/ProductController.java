ef8178175d97f15e88c80d98e65732079094d10d
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:47:57 +0400

    Add create Product endpoin

--- src/main/java/com/example/E_Commerce/Order/Platform/Controllers/ProductController.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/ProductController.java
@@ -2,6 +2,8 @@ package com.example.E_Commerce.Order.Platform.Controllers;
 
 import com.example.E_Commerce.Order.Platform.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService service;
+
+    @PostMapping("add")
+    public ProductDTO create(@RequestBody ProductDTO d) {
+
+        return service.create(d);
+    }
 
 }
