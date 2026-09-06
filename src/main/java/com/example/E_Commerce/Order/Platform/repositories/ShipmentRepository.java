6bcb280bfb920c87a87431b76233debfe7e344df
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:46:14 +0400

    Add method to get all active shipments

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/ShipmentRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/ShipmentRepository.java
@@ -3,7 +3,10 @@ package com.example.E_Commerce.Order.Platform.repositories;
 import com.example.E_Commerce.Order.Platform.entities.Shipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
+    List<Shipment> findAllByIsActiveTrue();
 
 }
 
