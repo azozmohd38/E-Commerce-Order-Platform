6a0e6a7721c48ca04eccc17e7736e8fe80a0feb1
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:32:45 +0400

    Create AddressRepository with JpaRepository

---
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/AddressRepository.java
@@ -0,0 +1,8 @@
+package com.example.E_Commerce.Order.Platform.repositories;
+
+import com.example.E_Commerce.Order.Platform.entities.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+}
