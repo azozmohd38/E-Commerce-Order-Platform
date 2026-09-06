066ac31e5e64249f7af1d251cab052e3e90afd1f
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 15:15:42 +0400

    Add create Shipment endpoint

--- src/main/java/com/example/E_Commerce/Order/Platform/Controllers/ShipmentController.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/ShipmentController.java
@@ -2,6 +2,8 @@ package com.example.E_Commerce.Order.Platform.Controllers;
 
 import com.example.E_Commerce.Order.Platform.service.ShipmentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShipmentController {
 
     private final ShipmentService service;
+    @PostMapping("add")
+    public ShipmentDTO create(@RequestBody ShipmentDTO d) {
+
+        return service.create(d);
+    }
 
 }
 
