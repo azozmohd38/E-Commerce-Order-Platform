d0c543e93a0682e6958055f6da45b2525744f88a
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:43:13 +0400

    Create CategoryController

---
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/StoreController.java
@@ -0,0 +1,10 @@
+package com.example.E_Commerce.Order.Platform.Controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("category")
+@RequiredArgsConstructor
+public class CategoryController {
