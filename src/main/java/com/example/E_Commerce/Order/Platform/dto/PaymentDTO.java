03e29b10a9038c97c62c89211b732c1016f3423f
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 16:20:35 +0400

    Add PaymentDTO fields

--- src/main/java/com/example/E_Commerce/Order/Platform/dto/PaymentDTO.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/dto/PaymentDTO.java
@@ -6,12 +6,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PaymentDTO {
 
+    private Long id;
+    private Double amount;
+    private String method;
+    private String status;
+    private Date paidDate;
 
 }
 
