a8b2229d2bff146e24eff823ee0f4002a8a99573
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 10:34:03 +0400

    Create Cart entity

--- src/main/java/com/example/E_Commerce/Order/Platform/entities/Address.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/entities/Address.java
@@ -2,7 +2,6 @@ package com.example.E_Commerce.Order.Platform.entities;
 
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
