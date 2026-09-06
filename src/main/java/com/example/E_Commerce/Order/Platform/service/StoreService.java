8b6c1cc50a681fa1a8e70f0de51e4b044ce0245a
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 12:15:15 +0400

    Add StoreRepository dependency

--- src/main/java/com/example/E_Commerce/Order/Platform/service/StoreService.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/StoreService.java
@@ -1,6 +1,6 @@
 package com.example.E_Commerce.Order.Platform.service;
 
-
+import com.example.E_Commerce.Order.Platform.dto.*;
 import com.example.E_Commerce.Order.Platform.entities.*;
 import com.example.E_Commerce.Order.Platform.repositories.*;
 import lombok.RequiredArgsConstructor;
@@ -16,4 +16,8 @@ import java.util.*;
 @Transactional
 public class StoreService implements CrudService<StoreDTO> {
 
+
+    private final StoreRepository repo;
 }
+
+
