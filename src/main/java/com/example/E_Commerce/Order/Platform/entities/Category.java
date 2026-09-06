7651d936b443bbda64b998babaa705eb26eb14eb
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 09:44:05 +0400

    add Category class with Gatter , Setter and extend

---
+++ src/main/java/com/example/E_Commerce/Order/Platform/entities/Category.java
@@ -0,0 +1,19 @@
+package com.example.E_Commerce.Order.Platform.entities;
+
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+
+public class Category extends BaseClass {
+
+
+
+
+
+}
