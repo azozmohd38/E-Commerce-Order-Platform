50c2567ed8d0906631b0c9f98dc6b6d658017211
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 16:21:03 +0400

    Add Payment entity to DTO conversion

--- src/main/java/com/example/E_Commerce/Order/Platform/dto/PaymentDTO.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/dto/PaymentDTO.java
@@ -1,6 +1,7 @@
 package com.example.E_Commerce.Order.Platform.dto;
 
 
+import com.example.E_Commerce.Order.Platform.entities.Payment;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,6 +21,16 @@ public class PaymentDTO {
     private String status;
     private Date paidDate;
 
+    public static PaymentDTO convertToDTO(Payment payment) {
+
+        return PaymentDTO.builder()
+                .id(payment.getId())
+                .amount(payment.getAmount())
+                .method(payment.getMethod())
+                .status(payment.getStatus())
+                .paidDate(payment.getPaidDate())
+                .build();
+    }
 }
 
 
