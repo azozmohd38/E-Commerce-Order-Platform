39f898bfd3f7e39615dddad9fab143afad48c1c8
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:57:50 +0400

    Add create Cart endpoint

--- src/main/java/com/example/E_Commerce/Order/Platform/Controllers/CartController.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/CartController.java
@@ -2,6 +2,8 @@ package com.example.E_Commerce.Order.Platform.Controllers;
 
 import com.example.E_Commerce.Order.Platform.service.CartService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,10 @@ public class CartController {
 
     private final CartService service;
 
+    @PostMapping("add")
+    public CartDTO create(@RequestBody CartDTO d) {
 
+        return service.create(d);
+    }
 
 }
