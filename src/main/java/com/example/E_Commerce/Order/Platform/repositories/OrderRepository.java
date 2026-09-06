dd517b9921ffa9cd27bdbd0d5c74fba0ff7b1d3b
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 17:11:07 +0400

    OrderRepository complited

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/OrderRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/OrderRepository.java
@@ -2,6 +2,8 @@ package com.example.E_Commerce.Order.Platform.repositories;
 
 import com.example.E_Commerce.Order.Platform.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +11,16 @@ import java.util.Optional;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findAllByIsActiveTrue();
+
     Optional<Order> findByIdAndIsActiveTrue(Long id);
 
+    @Query("""
+            SELECT o
+            FROM Order o
+            WHERE o.customer.id = :customerId
+            AND o.isActive = true
+            """)
+    List<Order> getOrdersByCustomer(
+            @Param("customerId") Long customerId
+    );
 }
