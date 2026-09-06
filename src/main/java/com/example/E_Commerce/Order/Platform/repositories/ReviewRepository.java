ee15dddded0df2d037f49ba6db33502be4f584e1
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:48:42 +0400

    Add method to get active review by ID

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/ReviewRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/ReviewRepository.java
@@ -4,11 +4,13 @@ import com.example.E_Commerce.Order.Platform.entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
 
     List<Review> findAllByIsActiveTrue();
+    Optional<Review> findByIdAndIsActiveTrue(Long id);
 
 }
 
