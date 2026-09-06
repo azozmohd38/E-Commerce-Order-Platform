8272c7bcadeef6f6ca956f323c8b668a6e044684
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:21:33 +0400

    Add CartItem CRUD operations

--- src/main/java/com/example/E_Commerce/Order/Platform/service/CartItemService.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/CartItemService.java
@@ -5,6 +5,9 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import static java.nio.file.Files.find;
+import static java.util.Collections.copy;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -12,6 +15,46 @@ public class CartItemService implements CrudService<CartItemDTO> {
 
     private final CartItemRepository repo;
 
+    public CartItemDTO create(CartItemDTO d) {
+
+        CartItem e = new CartItem();
+
+        copy(d, e);
+
+        e.setIsActive(true);
+
+        return CartItemDTO.convertToDTO(
+                repo.save(e)
+        );
+    }
+
+
+    public List<CartItemDTO> getAll() {
+
+        return CartItemDTO.convertToDTO(
+                repo.findAllByIsActiveTrue()
+        );
+    }
+
+
+    public CartItemDTO getById(Long id) {
+
+        return CartItemDTO.convertToDTO(
+                find(id)
+        );
+    }
+
+
+    public CartItemDTO update(Long id, CartItemDTO d) {
+
+        CartItem e = find(id);
+
+        copy(d, e);
+
+        return CartItemDTO.convertToDTO(
+                repo.save(e)
+        );
+    }
 
 }
 
