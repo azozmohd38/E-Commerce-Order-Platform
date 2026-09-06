d3ca33b5251e61fa6db4095ed67a02f5c47c3c49
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:30:32 +0400

    Add Payment CRUD operations

--- src/main/java/com/example/E_Commerce/Order/Platform/service/PaymentService.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/PaymentService.java
@@ -5,12 +5,55 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import static java.nio.file.Files.find;
+import static java.util.Collections.copy;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class PaymentService implements CrudService<PaymentDTO> {
 
     private final PaymentRepository repo;
+    public PaymentDTO create(PaymentDTO d) {
+
+        Payment e = new Payment();
+
+        copy(d, e);
+
+        e.setIsActive(true);
+
+        return PaymentDTO.convertToDTO(
+                repo.save(e)
+        );
+    }
+
+
+    public List<PaymentDTO> getAll() {
+
+        return PaymentDTO.convertToDTO(
+                repo.findAllByIsActiveTrue()
+        );
+    }
+
+
+    public PaymentDTO getById(Long id) {
+
+        return PaymentDTO.convertToDTO(
+                find(id)
+        );
+    }
+
+
+    public PaymentDTO update(Long id, PaymentDTO d) {
+
+        Payment e = find(id);
+
+        copy(d, e);
+
+        return PaymentDTO.convertToDTO(
+                repo.save(e)
+        );
+    }
 
 
 
