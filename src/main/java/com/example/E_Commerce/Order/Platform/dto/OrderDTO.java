1452233c27dc5bd469e6ae62e754b1d7556d91f6
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 16:15:50 +0400

    Add OrderDTO fields

--- src/main/java/com/example/E_Commerce/Order/Platform/dto/OrderDTO.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/dto/OrderDTO.java
@@ -6,10 +6,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderDTO {
+
+    private Long id;
+    private Date orderDate;
+    private String status;
+    private Double totalAmount;
+
 
 }
