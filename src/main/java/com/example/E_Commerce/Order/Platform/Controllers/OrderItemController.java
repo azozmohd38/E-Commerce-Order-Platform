343b6c0a34c66e073bf5c8d506e63191e42b8dae
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 15:08:22 +0400

    Create OrderItemController

---
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/OrderItemController.java
@@ -0,0 +1,13 @@
+package com.example.E_Commerce.Order.Platform.Controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+RestController
+@RequestMapping("orderItem")
+@RequiredArgsConstructor
+public class OrderItemController {
+
+}
+
