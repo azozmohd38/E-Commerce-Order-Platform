8c6c0386fb3f0c20550fd35e3175380903c51ed6
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:35:41 +0400

    Add method to get active cart by ID

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/CartRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/CartRepository.java
@@ -4,8 +4,12 @@ import com.example.E_Commerce.Order.Platform.entities.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     List<Cart> findAllByIsActiveTrue();
+    Optional<Cart> findByIdAndIsActiveTrue(Long id);
+
+
 }
