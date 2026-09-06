b004ce0edce187b3bb0472400c20578cf8d2e0bd
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:47:48 +0400

    Create ReviewRepository with JpaRepository

---
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.example.E_Commerce.Order.Platform.repositories;
+
+import com.example.E_Commerce.Order.Platform.entities.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}
+
