423d9f7b3f676fa79b858f720a8fddda5619e0e0
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:29:47 +0400

    Add method to get active product by ID

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/ProductRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/ProductRepository.java
@@ -4,10 +4,11 @@ import com.example.E_Commerce.Order.Platform.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
     List<Product> findAllByIsActiveTrue();
-
+    Optional<Product> findByIdAndIsActiveTrue(Long id);
 }
