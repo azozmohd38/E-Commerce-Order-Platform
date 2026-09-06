ee8dd1386e2d19d1ea11d0e7ab468a532c558ead
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:07:52 +0400

    Add CustomerRepository dependency

--- src/main/java/com/example/E_Commerce/Order/Platform/service/CustomerService.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/CustomerService.java
@@ -1,5 +1,6 @@
 package com.example.E_Commerce.Order.Platform.service;
 
+import com.example.E_Commerce.Order.Platform.repositories.CustomerRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,5 +9,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Transactional
 public class CustomerService implements CrudService<CustomerDTO> {
+
+    private final CustomerRepository repo;
+
 
 }
