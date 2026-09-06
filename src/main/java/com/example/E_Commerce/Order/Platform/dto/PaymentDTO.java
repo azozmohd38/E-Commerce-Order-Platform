7bce687abb3945cedb9e67fc50dc2348cdad1327
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 16:20:01 +0400

    Create PaymentDTO

---
+++ src/main/java/com/example/E_Commerce/Order/Platform/dto/PaymentDTO.java
@@ -0,0 +1,18 @@
+package com.example.E_Commerce.Order.Platform.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDTO {
+
+
+}
+
+
