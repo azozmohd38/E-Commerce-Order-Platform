42e29c84efe622f5bfe7c4068ceca5b5150fca75
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 15:14:42 +0400

    Create ShipmentController

---
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/ShipmentController.java
@@ -0,0 +1,14 @@
+package com.example.E_Commerce.Order.Platform.Controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("shipment")
+@RequiredArgsConstructor
+public class ShipmentController {
+
+
+}
+
