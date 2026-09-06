a1ee30122bebec5c017300cae66a9c60e8c82c2b
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 15:25:45 +0400

    Create StoreDTO

---
+++ src/main/java/com/example/E_Commerce/Order/Platform/dto/StoreDTO.java
@@ -0,0 +1,13 @@
+package com.example.E_Commerce.Order.Platform.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+public class StoreDTO {
+
+
+}
