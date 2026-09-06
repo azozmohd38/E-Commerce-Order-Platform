eeff868b57cda99eb702840cd37ead16ae55d2d0
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 15:28:58 +0400

    Add Category entity to DTO conversion

--- src/main/java/com/example/E_Commerce/Order/Platform/dto/CategoryDTO.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/dto/CategoryDTO.java
@@ -1,5 +1,6 @@
 package com.example.E_Commerce.Order.Platform.dto;
 
+import com.example.E_Commerce.Order.Platform.entities.Category;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,5 +15,13 @@ public class CategoryDTO {
     private String name;
     private String description;
 
+    public static CategoryDTO convertToDTO(Category category) {
+
+        return CategoryDTO.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .description(category.getDescription())
+                .build();
+    }
 }
 
