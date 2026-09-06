762297b4dc9f59ee5f219b07311c2ef3661f2539
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:28:40 +0400

    Add method to get all active products

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/ProductRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/ProductRepository.java
@@ -3,6 +3,11 @@ package com.example.E_Commerce.Order.Platform.repositories;
 import com.example.E_Commerce.Order.Platform.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    List<Product> findAllByIsActiveTrue();
 
 }
