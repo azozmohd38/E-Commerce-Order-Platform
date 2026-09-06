8d5252d7abec12fdb8041df9d6238954269d4809
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 10:53:48 +0400

    Add Payment and Order relationship

--- src/main/java/com/example/E_Commerce/Order/Platform/entities/Payment.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/entities/Payment.java
@@ -2,6 +2,7 @@ package com.example.E_Commerce.Order.Platform.entities;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,6 +18,8 @@ public class Payment extends BaseClass{
     private String status;
     private Date paidDate;
 
+@OneToOne
+    private Order order;
 
 
 
