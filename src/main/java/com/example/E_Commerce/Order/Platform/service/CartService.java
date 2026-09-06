e6e3fa6d4fab48397d6952398c76485c87668d07
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:18:22 +0400

    Add Cart CRUD operations

--- src/main/java/com/example/E_Commerce/Order/Platform/service/CartService.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/CartService.java
@@ -1,9 +1,13 @@
 package com.example.E_Commerce.Order.Platform.service;
 
+import com.example.E_Commerce.Order.Platform.entities.Cart;
 import com.example.E_Commerce.Order.Platform.repositories.CartRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static java.nio.file.Files.find;
+import static java.util.Collections.copy;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +15,44 @@ import org.springframework.stereotype.Service;
 public class CartService implements CrudService<CartDTO> {
 
     private final CartRepository repo;
+
+    public CartDTO create(CartDTO d) {
+
+        Cart e = new Cart();
+
+        copy(d, e);
+
+        e.setIsActive(true);
+
+        return CartDTO.convertToDTO(repo.save(e));
+    }
+
+
+    public List<CartDTO> getAll() {
+
+        return CartDTO.convertToDTO(
+                repo.findAllByIsActiveTrue()
+        );
+    }
+
+
+    public CartDTO getById(Long id) {
+
+        return CartDTO.convertToDTO(
+                find(id)
+        );
+    }
+
+
+    public CartDTO update(Long id, CartDTO d) {
+
+        Cart e = find(id);
+
+        copy(d, e);
+
+        return CartDTO.convertToDTO(
+                repo.save(e)
+        );
+    }
 }
 
