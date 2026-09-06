6728222fe68b326d5b5de6029a36636241f7a8de
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 15:28:11 +0400

    Create CategoryDTO

---
+++ src/main/java/com/example/E_Commerce/Order/Platform/dto/CategoryDTO.java
@@ -0,0 +1,15 @@
+package com.example.E_Commerce.Order.Platform.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+public class CategoryDTO {
+
+
+}
+
