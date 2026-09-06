1d17aded0252251e8069e985c46304d0e3f5749d
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:25:56 +0400

    Add method to get all active categories

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/CategoryRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/CategoryRepository.java
@@ -3,6 +3,11 @@ package com.example.E_Commerce.Order.Platform.repositories;
 import com.example.E_Commerce.Order.Platform.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findAllByIsActiveTrue();
+
 
 }
