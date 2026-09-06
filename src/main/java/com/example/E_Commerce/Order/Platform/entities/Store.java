0de5ab391cab1895e157b9384a9e59ac7964c3ac
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 09:28:23 +0400

    add Getter , Setter and extend in Store class

---
+++ src/main/java/com/example/E_Commerce/Order/Platform/entities/Store.java
@@ -0,0 +1,15 @@
+package com.example.E_Commerce.Order.Platform.entities;
+
+
+import jakarta.persistence.Entity;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Setter
+
+public class Store extends BaseClass {
+
+
+
+}
