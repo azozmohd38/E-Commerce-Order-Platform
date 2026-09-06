f12360a34aa5c850e4ca665cf9b9ebf82c3ebc60
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 09:34:20 +0400

    add Product relationshipin  Store class

--- src/main/java/com/example/E_Commerce/Order/Platform/entities/Store.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/entities/Store.java
@@ -1,8 +1,13 @@
 package com.example.E_Commerce.Order.Platform.entities;
 
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jdk.jfr.Category;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Setter
@@ -12,6 +17,13 @@ public class Store extends BaseClass {
 
       private String name;
       private String location;
+
+
+@OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+private List<Product> products;
+
+
+
 
 
 }
