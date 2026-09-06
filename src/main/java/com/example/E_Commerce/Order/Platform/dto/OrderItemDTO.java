09a58c01a308376aecc3f007692c08314aae54eb
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 16:18:31 +0400

    Add OrderItem entity to DTO conversion

--- src/main/java/com/example/E_Commerce/Order/Platform/dto/OrderItemDTO.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/dto/OrderItemDTO.java
@@ -1,6 +1,7 @@
 package com.example.E_Commerce.Order.Platform.dto;
 
 
+import com.example.E_Commerce.Order.Platform.entities.OrderItem;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,16 @@ public class OrderItemDTO {
     private Long id;
     private Integer quantity;
     private Double unitPrice;
+
+    public static OrderItemDTO convertToDTO(OrderItem orderItem) {
+
+        return OrderItemDTO.builder()
+                .id(orderItem.getId())
+                .quantity(orderItem.getQuantity())
+                .unitPrice(orderItem.getUnitPrice())
+                .build();
+    }
+
 
 
 }
