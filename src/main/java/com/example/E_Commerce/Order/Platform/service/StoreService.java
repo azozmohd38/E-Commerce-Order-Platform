8a78c3d075cc5fc3fff4552f201cc15f848b94d2
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 12:14:43 +0400

    modifi and change in StoreService class

--- src/main/java/com/example/E_Commerce/Order/Platform/service/StoreService.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/StoreService.java
@@ -1,67 +1,19 @@
 package com.example.E_Commerce.Order.Platform.service;
 
-import com.example.E_Commerce.Order.Platform.entities.Store;
-import com.example.E_Commerce.Order.Platform.repositories.StoreRepository;
-import jakarta.transaction.Transactional;
+
+import com.example.E_Commerce.Order.Platform.entities.*;
+import com.example.E_Commerce.Order.Platform.repositories.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import static java.nio.file.Files.find;
+import java.util.*;
 
+
+// Git Commit: Create StoreService
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class StoreService implements CrudService<StoreDTO> {
-
-    private final StoreRepository repo;
-
-    @Override
-    public StoreDTO create(StoreDTO d) {
-        Store e = new Store();
-        copy(d, e);
-        e.setIsActive(true);
-        return StoreDTO.convertToDTO(repo.save(e));
-
-
-    }
-    @Override
-    public List<StoreDTO> getAll() {
-        return StoreDTO.convertToDTO(
-                repo.findAllByIsActiveTrue()
-        );
-    }
-    @Override
-    public StoreDTO getById(Long id) {
-        return StoreDTO.convertToDTO(
-                find(id)
-        );
-    }
-    @Override
-    public StoreDTO update(Long id, StoreDTO d) {
-        Store e = find(id);
-        copy(d, e);
-        return StoreDTO.convertToDTO(
-                repo.save(e)
-        );
-    }
-
-    @Override
-    public void delete(Long id) {
-        Store e = find(id);
-        e.setIsActive(false);
-        repo.save(e);
-    }
-    Store find(Long id) {
-
-        return EntityHelper.active(
-                repo,
-                id,
-                "Store"
-        );
-    }
-
-
-
-
 
 }
