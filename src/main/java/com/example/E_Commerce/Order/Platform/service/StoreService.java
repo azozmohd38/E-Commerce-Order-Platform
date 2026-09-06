6eb932806bae08adbc9a8a31fdd1c729b145b99a
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 12:03:49 +0400

    Add create Store operation

--- src/main/java/com/example/E_Commerce/Order/Platform/service/StoreService.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/StoreService.java
@@ -1,5 +1,6 @@
 package com.example.E_Commerce.Order.Platform.service;
 
+import com.example.E_Commerce.Order.Platform.entities.Store;
 import com.example.E_Commerce.Order.Platform.repositories.StoreRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,16 @@ import org.springframework.stereotype.Service;
 public class StoreService implements CrudService<StoreDTO> {
 
     private final StoreRepository repo;
+
+    @Override
+    public StoreDTO create(StoreDTO d) {
+        Store e = new Store();
+        copy(d, e);
+        e.setIsActive(true);
+        return StoreDTO.convertToDTO(repo.save(e));
+
+
+    }
 
 
 
