fb6105ef4ab0a6cb355da149038d31e8840f9adb
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 15:16:06 +0400

    Add get all Shipments endpoint

--- src/main/java/com/example/E_Commerce/Order/Platform/Controllers/ShipmentController.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/ShipmentController.java
@@ -2,10 +2,7 @@ package com.example.E_Commerce.Order.Platform.Controllers;
 
 import com.example.E_Commerce.Order.Platform.service.ShipmentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("shipment")
@@ -17,6 +14,11 @@ public class ShipmentController {
     public ShipmentDTO create(@RequestBody ShipmentDTO d) {
 
         return service.create(d);
+    }
+    @GetMapping("getAll")
+    public List<ShipmentDTO> getAll() {
+
+        return service.getAll();
     }
 
 }
