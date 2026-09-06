fe2eb6eac7ddb183fa5c37a34766cd49c983a372
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 16:26:38 +0400

    Add Review list to DTO conversion

--- src/main/java/com/example/E_Commerce/Order/Platform/dto/ReviewDTO.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/dto/ReviewDTO.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @Builder
@@ -27,6 +29,12 @@ public class ReviewDTO {
                 .comment(review.getComment())
                 .reviewDate(review.getReviewDate())
                 .build();
+    }
+    public static List<ReviewDTO> convertToDTO(List<Review> reviews) {
+
+        return reviews.stream()
+                .map(ReviewDTO::convertToDTO)
+                .collect(Collectors.toList());
     }
 
 
