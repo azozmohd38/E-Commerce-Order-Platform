67630640a665719d2ff5ffe28302dd5ca82490db
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:08:55 +0400

    Add Customer CRUD operations (getAll)

--- src/main/java/com/example/E_Commerce/Order/Platform/service/CustomerService.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/service/CustomerService.java
@@ -5,12 +5,31 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import static java.util.Collections.copy;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class CustomerService implements CrudService<CustomerDTO> {
 
     private final CustomerRepository repo;
+
+    public CustomerDTO create(CustomerDTO d) {
+
+        Customer e = new Customer();
+
+        copy(d, e);
+
+        e.setIsActive(true);
+
+        return CustomerDTO.convertToDTO(repo.save(e));
+    }
+    public List<CustomerDTO> getAll() {
+
+        return CustomerDTO.convertToDTO(
+                repo.findAllByIsActiveTrue()
+        );
+    }
 
 
 }
