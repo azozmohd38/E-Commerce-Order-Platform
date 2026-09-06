c3358e2763bebbc76adfd99659ee88a4cc62b734
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:42:05 +0400

    Add method to get all active order items

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/OrderItemRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/OrderItemRepository.java
@@ -3,7 +3,11 @@ package com.example.E_Commerce.Order.Platform.repositories;
 import com.example.E_Commerce.Order.Platform.entities.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    List<OrderItem> findAllByIsActiveTrue();
 
 
 }
