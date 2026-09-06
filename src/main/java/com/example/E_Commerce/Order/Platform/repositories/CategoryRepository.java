1e048abeaa6b5d887016dcffbca505d16e225103
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:26:49 +0400

    Add method to get active category by ID

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/CategoryRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/CategoryRepository.java
@@ -4,10 +4,14 @@ import com.example.E_Commerce.Order.Platform.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     List<Category> findAllByIsActiveTrue();
+    Optional<Category> findByIdAndIsActiveTrue(Long id);
+
+
 
 
 }
