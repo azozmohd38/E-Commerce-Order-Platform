e104e5b8465a853e280f73b1f32c9cf971c83a7b
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 10:53:12 +0400

    Add Payment basic fields

--- src/main/java/com/example/E_Commerce/Order/Platform/entities/Payment.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/entities/Payment.java
@@ -5,10 +5,19 @@ import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Entity
 @Getter
 @Setter
 public class Payment extends BaseClass{
+
+    private Double amount;
+    private String method;
+    private String status;
+    private Date paidDate;
+
+
 
 
 
