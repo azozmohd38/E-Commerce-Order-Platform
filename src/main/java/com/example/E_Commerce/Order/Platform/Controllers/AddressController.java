2b67ac23ed220f79a2a0fb9694baa93c47edbf81
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 14:56:25 +0400

    Add soft delete Address endpoint

--- src/main/java/com/example/E_Commerce/Order/Platform/Controllers/AddressController.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/Controllers/AddressController.java
@@ -33,7 +33,9 @@ public class AddressController {
 
         return service.update(id, d);
     }
+    @DeleteMapping("delete")
+    public void delete(@RequestParam Long id) {
 
-
-
+        service.delete(id);
+    }
 }
