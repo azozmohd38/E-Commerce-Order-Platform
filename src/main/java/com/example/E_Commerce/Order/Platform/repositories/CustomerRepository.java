800795dd16db0c07490f1e30d1a264ce442f2665
Abdulaziz <azozmohd38@gmail.com>
2026-09-06 11:31:54 +0400

    Add method to get active customer by ID

--- src/main/java/com/example/E_Commerce/Order/Platform/repositories/CustomerRepository.java
+++ src/main/java/com/example/E_Commerce/Order/Platform/repositories/CustomerRepository.java
@@ -4,10 +4,13 @@ import com.example.E_Commerce.Order.Platform.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 
     List<Customer> findAllByIsActiveTrue();
+    Optional<Customer> findByIdAndIsActiveTrue(Long id);
+
 
 }
